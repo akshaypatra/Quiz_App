01fae44aa0540d9b3ac3e0db0f25f1e1125f532e
Aditya Shinde <aditya.manoj.shinde@gmail.com>
2025-03-14 23:34:32 +0530

    minor changes to backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// IMPORTANT: Add build folder from frontend to serve static files
 // Serve static files from the build folder
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -12,5 +13,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server 🚀 is running on http://localhost:${PORT}`);
 });
